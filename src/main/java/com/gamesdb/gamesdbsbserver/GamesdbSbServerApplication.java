0c26575b3f7e7d418bb6b16e69ce38d8937a460f
Metal Face <50063043+metalfacesec@users.noreply.github.com>
2021-12-23 20:53:55 -0500

    Pushing up template

---
+++ src/main/java/com/gamesdb/gamesdbsbserver/GamesdbSbServerApplication.java
@@ -0,0 +1,13 @@
+package com.gamesdb.gamesdbsbserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesdbSbServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesdbSbServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamesdb/gamesdbsbserver/GamesdbSbServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamesdb.gamesdbsbserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesdbSbServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
